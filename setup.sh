ee2c75558ea72e8b85a8e41cbbe509a7e9f01457
Pascal2511 <115885966+Pascal2511@users.noreply.github.com>
2023-04-13 20:29:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,6 @@ sudo apt-get install build-essential python-dev python-openssl git
 sudo pip3 install adafruit-circuitpython-dht
 sudo apt-get install libgpiod2
 sudo apt-get install python3-pip
-
+chmod +x DHT_22.py
 echo "Installation complete! Thanks for waiting"
+sudo python DHT_22.py
