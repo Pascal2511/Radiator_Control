4503f7625dfc53f1934bea60cdcab3c42d20a239
Pascal2511 <115885966+Pascal2511@users.noreply.github.com>
2023-04-13 20:27:11 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to the Install-assistant!"
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install build-essential python-dev python-openssl git
+sudo pip3 install adafruit-circuitpython-dht
+sudo apt-get install libgpiod2
+sudo apt-get install python3-pip
+
+echo "Installation complete! Thanks for waiting"
