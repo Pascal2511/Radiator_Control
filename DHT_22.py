5e273b695b8a19d409b06d58dace8bc093c106a2
Pascal2511 <115885966+Pascal2511@users.noreply.github.com>
2023-04-17 16:46:35 +0200

    Update DHT_22.py

--- DHT_22.py
+++ DHT_22.py
@@ -2,28 +2,6 @@ import time
 import board
 import adafruit_dht
 import RPi.GPIO as GPIO
-dhtDevice = adafruit_dht.DHT11(board.D4)
-#dhtDevice = adafruit_dht.DHT22(board.D4)
-
-min_temp = 10
-max_temp = 25
-heater   = 17
-delay    = 0.5
-
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(heater, GPIO.OUT)
-
-GPIO.output(heater, True)
-time.sleep(1)
-GPIO.output(heater, False)
-
-GPIO.cleanup()
-
-import time
-import board
-import adafruit_dht
-import RPi.GPIO as GPIO
 
 dhtDevice = adafruit_dht.DHT11(board.D4)
 
