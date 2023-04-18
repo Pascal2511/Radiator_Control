3e68ebb09bf46f03f6227ba8a3602a603a9d47b8
Pascal2511 <115885966+Pascal2511@users.noreply.github.com>
2023-04-18 09:38:02 +0200

    Update daemonSetup.sh

--- daemonSetup.sh
+++ daemonSetup.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "Welcome to the daemon-setup!"
+echo "Creating a linux daemon!"
+sudo cp radiator.service /etc/systemd/system/
+sudo cp DHT_22.py /opt/
+echo "Enable the linux daemon by typing:"
+sudo systemctl daemon-reload
+sudo systemctl enable radiator.service
+sudo systemctl start radiator.service
+echo "Checking the status of the service:"
+sudo systemctl status radiator.service
